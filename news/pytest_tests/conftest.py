f21676a4939e8c9c9bd3bb967bee4fffddba2e84
Firuz Dadabaev <firuzdadabaev.job@gmail.com>
2025-02-10 23:20:57 +0500

    Finished tests for content

---
+++ news/pytest_tests/conftest.py
@@ -0,0 +1,93 @@
+import pytest
+from datetime import datetime, timedelta
+
+from django.test.client import Client
+from django.conf import settings
+from django.utils import timezone
+
+from news.models import News, Comment
+
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Author')
+
+
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Not Author')
+
+
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def not_author_client(not_author):
+    client = Client()
+    client.force_login(not_author)
+    return client
+
+
+@pytest.fixture
+def news():
+    news = News.objects.create(title='Заголовок', text='Текст')
+    return news
+
+
+@pytest.fixture
+def comment(author, news):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def pk_for_args_news(author, news):
+    return (news.pk, )
+
+
+@pytest.fixture
+def pk_for_args_comment(comment):
+    return (comment.pk, )
+
+
+@pytest.fixture
+def news_count(news):
+    return News.objects.count()
+
+
+@pytest.fixture
+def ten_news():
+    today = datetime.today()
+    all_news = [
+        News(
+            title=f'Новость {index}',
+            text='Просто текст.',
+            date=today - timedelta(days=index)
+        )
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+    ]
+    News.objects.bulk_create(all_news)
+
+
+@pytest.fixture
+def ten_news_count(author, ten_news):
+    return (News.objects.count() - 1)
+
+
+@pytest.fixture
+def ten_comments(author, news):
+    now = timezone.now()
+    for index in range(10):
+        comment = Comment.objects.create(
+            news=news, author=author, text=f'Tекст {index}',
+        )
+        comment.created = now + timedelta(days=index)
+        comment.save()
---
+++ news/pytest_tests/test_content.py
@@ -0,0 +1,57 @@
+import pytest
+
+from django.urls import reverse
+
+from news.forms import CommentForm
+
+HOME_URL = 'news:home'
+
+
+def test_news_count(client, ten_news_count):
+    url = reverse(HOME_URL)
+    response = client.get(url)
+
+    object_list = response.context['object_list']
+
+    assert object_list.count() == ten_news_count
+
+
+def test_ten_news_order(ten_news_count, client):
+    url = reverse(HOME_URL)
+    response = client.get(url)
+
+    object_list = response.context['object_list']
+    all_date = [news.date for news in object_list]
+    sorted_dates = sorted(all_date, reverse=True)
+
+    assert object_list.count() == ten_news_count
+    assert all_date == sorted_dates
+
+
+def test_ten_comment_order(client, ten_comments, pk_for_args_news):
+    url = reverse('news:detail', args=pk_for_args_news)
+    response = client.get(url)
+
+    assert 'news' in response.context
+    news = response.context['news']
+    all_comments = news.comment_set.all()
+    all_timestamps = [comment.created for comment in all_comments]
+    sorted_timestamps = sorted(all_timestamps)
+
+    assert all_comments.count() == 10
+    assert all_timestamps == sorted_timestamps
+
+
+def test_anonymous_client_has_no_form(client, pk_for_args_news):
+    url = reverse('news:detail', args=pk_for_args_news)
+    response = client.get(url)
+
+    assert 'form' not in response.context
+
+
+def test_authorized_client_has_form(author_client, pk_for_args_news):
+    url = reverse('news:detail', args=pk_for_args_news)
+    response = author_client.get(url)
+
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], CommentForm)
